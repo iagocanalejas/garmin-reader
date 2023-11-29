2db15eeeaf85f37caa302efe7b68b53dc0b92b49
Iagocanalejas <iagocanalejas@gmail.com>
2023-11-29 13:47:52 +0100

    retrieve activities

--- src/garmin.rs
+++ src/garmin.rs
@@ -1,25 +1,92 @@
-use reqwest;
+use chrono::Utc;
+use serde::Deserialize;
 
-// bearer and jwt expire quite quickly
 pub struct GarminClient {
-    pub bearer: String,
+    pub client: reqwest::Client,
+    pub username: String,
+    pub password: String,
+    pub session_id: String,
     pub jwt: String,
+    pub bearer: String,
 }
 
-pub async fn load_activities(garmin: &GarminClient) {
-    let client = reqwest::Client::new();
-    let res = client
-        .get("https://connect.garmin.com/activitylist-service/activities/search/activities")
-        .header("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")
-        .header("cookie", format!("JWT_FGP={}", garmin.jwt))
-        .header("authorization", format!("Bearer {}", garmin.bearer))
+#[derive(Debug, Deserialize)]
+pub struct ActivityType {
+    #[serde(rename(deserialize = "typeKey"))]
+    pub key: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Activity {
+    #[serde(rename(deserialize = "activityId"))]
+    pub id: u64,
+    #[serde(rename(deserialize = "activityName"))]
+    pub name: String,
+    #[serde(rename(deserialize = "activityType"))]
+    pub atype: ActivityType,
+
+    pub distance: Option<f64>,
+    #[serde(rename(deserialize = "movingDuration"))]
+    pub duration: f64,
+
+    #[serde(rename(deserialize = "averageSpeed"))]
+    pub average_speed: Option<f64>,
+    #[serde(rename(deserialize = "maxSpeed"))]
+    pub max_speed: Option<f64>,
+    #[serde(rename(deserialize = "avgRespirationRate"))]
+    pub average_respiration_rate: Option<f64>,
+
+    #[serde(rename(deserialize = "averageHR"))]
+    pub average_hr: Option<f32>,
+    #[serde(rename(deserialize = "maxHR"))]
+    pub max_hr: Option<f32>,
+    #[serde(rename(deserialize = "aerobicTrainingEffect"))]
+    pub aerobic_training_effect: Option<f32>,
+    #[serde(rename(deserialize = "anaerobicTrainingEffect"))]
+    pub anaerobic_training_effect: Option<f32>,
+}
+
+// TODO: find races
+// TODO: iterate over all the races
+pub async fn load_races(garmin: GarminClient) {
+    // "rowing_v2"
+    let params = [
+        ("search", "".to_string()),
+        ("activityType", "rowing_v2".to_string()),
+        ("limit", 20.to_string()),
+        ("start", 0.to_string()),
+        ("_", Utc::now().timestamp_millis().to_string()),
+    ];
+    let url = reqwest::Url::parse_with_params(
+        "https://connect.garmin.com/activitylist-service/activities/search/activities",
+        params,
+    );
+    let results: Vec<Activity> = garmin
+        .client
+        .get(url.unwrap())
+        .header("Cookie", format!("SESSIONID={}; JWT_FGP={}", garmin.session_id, garmin.jwt))
+        .header("Authorization", format!("Bearer {}", garmin.bearer))
+        .header("Accept", "application/json")
         .header("di-backend", "connectapi.garmin.com")
+        .header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36")
         .send()
+        .await
+        .unwrap()
+        .json()
         .await
         .unwrap();
 
-    println!(": {:?}", format!("JWT_FPG={}", garmin.jwt));
-    println!(": {:?}", format!("Bearer {}", garmin.bearer));
-    println!(": {:?}", res.status());
-    println!(": {:?}", res.text().await.unwrap());
+    let filtered_results: Vec<&Activity> = results
+        .iter()
+        .filter(|activity| activity.atype.key == "rowing_v2")
+        .collect();
+
+    println!("{:?}", filtered_results);
+}
+
+pub async fn login(garmin: &mut GarminClient) {
+    println!("SESSIONID={}", garmin.session_id);
+
+    // TODO: retrieve the bearer
+    garmin.password = "".to_string(); // avoid having the password loaded more than the strictly required
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod garmin;
+
 use clap::Parser;
 use dotenv::dotenv;
 
@@ -15,10 +16,15 @@ async fn main() {
     dotenv().ok();
 
     // let args = Cli::parse();
-    let client = GarminClient {
-        bearer: std::env::var("GARMIN_BEARER").expect("GARMIN_BEARER must be set."),
+    let mut client = GarminClient {
+        client: reqwest::Client::builder().build().unwrap(),
+        username: std::env::var("GARMIN_USER").expect("GARMIN_USER must be set."),
+        password: std::env::var("GARMIN_PASSWORD").expect("GARMIN_PASSWORD must be set."),
+        session_id: std::env::var("GARMIN_SESSION").expect("GARMIN_SESSION must be set."),
         jwt: std::env::var("GARMIN_JWT").expect("GARMIN_JWT must be set."),
+        bearer: std::env::var("GARMIN_BEARER").expect("GARMIN_BEARER must be set."),
     };
 
-    garmin::load_activities(&client).await;
+    garmin::login(&mut client).await;
+    garmin::load_races(client).await;
 }
